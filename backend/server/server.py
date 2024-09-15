3664b610d53ad5d19680a5138854e0430a32e4cf
yonatand <yonadolg@gmail.com>
2024-09-15 12:08:36 +0300

    fix: import error

--- backend/server/server.py
+++ backend/server/server.py
@@ -1,12 +1,12 @@
 import json
 import sys
 import threading
+from datetime import datetime
 from socket import AF_INET, SOCK_STREAM, socket
 from typing import Tuple
 
 from logs import logger
 
-from .clientFunctions import handle_client
 from .consts import MAX_SOCKETS
 from .socketManager import SocketManager
 
@@ -21,6 +21,47 @@ def handle_client_disconnect(client_address: Tuple[str, int]):
 
 
 socket_manager = SocketManager(handle_client_disconnect)
+
+
+# TODO: exception handling
+def handle_read_client(client_address: Tuple[str, int], data_str: str):
+    data = json.loads(data_str)
+    if data["event"] == "login" and isinstance(data["username"], str):
+        username_list = socket_manager.read_all_data_by_key("username")
+        if data["username"] in username_list:
+            socket_manager.write_to_socket(
+                client_address,
+                json.dumps({"event": "error", "message": "Duplicate username"}),
+            )
+            return
+        socket_manager.write_client_data(client_address, "username", data["username"])
+        socket_manager.broadcast(
+            json.dumps({"event": "login", "username": data["username"]}), client_address
+        )
+        username_list.append(data["username"])
+        socket_manager.write_to_socket(
+            client_address, json.dumps({"event": "userList", "data": username_list})
+        )
+    elif data["event"] == "message" and isinstance(data["data"], str):
+        username: str = socket_manager.read_client_data(client_address, "username")
+        socket_manager.broadcast(
+            json.dumps(
+                {
+                    "event": "message",
+                    "username": username,
+                    "data": data["data"],
+                    "time": datetime.now().isoformat(),
+                }
+            ),
+        )
+
+
+def handle_client(
+    client_socket: socket,
+    client_address: Tuple[str, int],
+):
+    socket_manager.register_socket(client_address, client_socket)
+    socket_manager.read_from_socket(client_address, handle_read_client)
 
 
 def get_port():
